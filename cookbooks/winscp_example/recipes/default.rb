5a52ccd9a0cc67cfb96cfbaa0a189174d8aae9b1
Pradeep Kumar <pbuditi@gmail.com>
2016-06-28 20:49:37 +0530

    winscp changes

--- cookbooks/winscp_example/recipes/default.rb
+++ cookbooks/winscp_example/recipes/default.rb
@@ -16,20 +16,28 @@ node["sftp-folders"].each do |folderkey, folderval|
 	end
 end
 	
-template "#{node["sftp-folders"]["bin"]}\\remoteftp.bat" do
+template "#{node["sftp-folders"]["bin"]}/remotesftp.bat" do
 	source "remoteftp.erb"
-	mode "0644"
+#	rights :read, 'Everyone'
+#   rights :full_control, 'Administrators'
 		variables(
 				:ftpuser => "#{node["sftp"]["user"]}",
 				:ftppwd => "#{node["sftp"]["password"]}",
 				:ftppath => "#{node["sftp"]["target"]}",
-				:ftpserver => "#{node["sftp"]["server"]}"
+				:ftpserver => "#{node["sftp"]["server"]}",
+				:sftp_home => "#{node["sftp-home"]}"
 			)
+end
+
+template "#{node["sftp-folders"]["bin"]}/autocleanup.bat" do
+	source "autocleanup.erb"
+#	rights :read, 'Everyone'
+#   rights :full_control, 'Administrators'
 end
 
 
 # create windows scheduler if it doesn't eist already
-execute 'schtasks' do
-	command 'schtasks /create /sc minute /mo 30 /tr #{node["sftp-folders"]["bin"]}/cleanupsftp.bat /tn #{schedule_task}'
-	not if 'schtasks /query /tn #{schedule_task} /NH /FO CSV'
-end
+#execute 'schtasks' do
+#	command 'schtasks /create /sc minute /mo 30 /tr #{node["sftp-folders"]["bin"]}/cleanupsftp.bat /tn #{schedule_task}'
+#	not_if 'schtasks /query /tn #{schedule_task} /NH /FO CSV'
+#end
