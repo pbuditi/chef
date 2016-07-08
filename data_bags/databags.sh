8532c11bbd12484b9c669e42cdadae2a592d73b4
Pradeep Kumar <pbuditi@gmail.com>
2016-07-08 17:43:52 +0530

    databags automation

--- data_bags/databags.sh
+++ data_bags/databags.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
+databag_file="databags.txt"
+data_bags_list=`knife data bag list 2>/dev/null`
+databags={$data_bags_list}
 
-knife data bag create user 
-knife data bag create group 
-knife data bag create mysql
-knife data bag create app
+
+lines=`cat $databag_file`
+for line in $lines; do
+		found=0
+		for i in ${data_bags_list[@]}
+		do 
+			#echo "checking for $i in $line"
+			if [ "$i" == "$line" ] ; then
+				found=1
+        	fi
+        done
+        if [ $found == 0 ] ; then
+        	echo "creating data bag $line..."
+        	output=`knife data bag create $line` 2>/dev/null
+        fi
+done
+
+
+containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
+
+#knife data bag create user 
+#knife data bag create group 
+#knife data bag create mysql
+#knife data bag create app
