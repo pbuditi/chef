fa73bcee519ec709bf18add727a3d02500eaf3cd
pbuditi <pbuditi@gmail.com>
2016-12-11 22:07:13 +0530

    recipe change

--- cookbooks/winscp_example/recipes/default.rb
+++ cookbooks/winscp_example/recipes/default.rb
@@ -23,8 +23,8 @@ template "#{node["sftp-folders"]["bin"]}/remotesftp.bat" do
 		variables(
 				:ftpuser => "#{node["sftp"]["user"]}",
 				:ftppwd => "#{node["sftp"]["password"]}",
-				:ftppathOut => "#{node["sftp"]["target"]['outgoing']}",
-				:ftppathIn => "#{node["sftp"]["target"]['incoming']}",
+				:ftppathOut => "#{node["sftp"]["target"]["outgoing"]}",
+				:ftppathIn => "#{node["sftp"]["target"]["incoming"]}",
 				:ftpserver => "#{node["sftp"]["server"]}",
 				:sftp_home => "#{node["sftp-home"]}"
 			)
